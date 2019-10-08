7653f910659fce73d10c40a07ca32c95fc4a0ad1
MarkenJaden <jjsch1410@gmail.com>
2019-10-08 20:28:35 +0200

    Readmeeee~ •ᴗ•

---
+++ src/main/java/net/landofrails/learningforge/init/TutorialBlocks.java
@@ -0,0 +1,13 @@
+package net.landofrails.learningforge.init;
+
+import net.landofrails.learningforge.LearningForge;
+
+import net.minecraft.block.Block;
+import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
+
+@ObjectHolder(LearningForge.MODID)
+public class TutorialBlocks {
+
+	public static final Block BASIC_BLOCK = null;
+	
+}
